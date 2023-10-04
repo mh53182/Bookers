134419162b7d8dd556e002f6997409c4ef1ed785
mh53182 <marie.i.077145@gmail.com>
2023-10-04 15:11:31 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,23 @@
 class BooksController < ApplicationController
-  def new
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path
   end
-  
+
   def index
+    @book = Book.new
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end

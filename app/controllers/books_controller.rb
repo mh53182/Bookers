1eef3f565d1f28e281de433a8d2d7f68b93d39b6
mh53182 <marie.i.077145@gmail.com>
2023-10-09 03:20:39 +0000

    [Add]レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created"
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
       # 詳細画面へリダイレクトするには固定のURL"book_path"ではなく"book_path(@book.id)"じゃないとダメ。
     else
@@ -28,14 +28,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    flash[:notice] = "Book was successfully updated"
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully destroyed"
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
